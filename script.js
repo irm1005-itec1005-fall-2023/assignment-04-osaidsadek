614a47563d7e98eb9c96cab3d9616ec9ec5bb233
osaidsadek <144632318+osaidsadek@users.noreply.github.com>
2023-11-28 21:37:38 +0000

    ADDED

--- script.js
+++ script.js
@@ -6,48 +6,21 @@ function addTodo() {
     if (text) {
         var li = document.createElement('li');
         li.className = 'todo-item';
-        li.innerHTML = `<span>${text}</span> <button class="delete-btn">Delete</button>`;
-        li.querySelector('.delete-btn').addEventListener('click', function() {
-            li.remove();
-        });
-        li.addEventListener('click', function() {
+        li.textContent = text;
+        li.addEventListener('click', function () {
             this.classList.toggle('completed');
         });
         document.getElementById('todo-list').appendChild(li);
         input.value = '';
+    } else {
+        alert('Please enter a valid task.');
     }
 }
 
-document.querySelectorAll('.filter-btn').forEach(button => {
-    button.addEventListener('click', function() {
-        document.querySelectorAll('.filter-btn').forEach(btn => btn.classList.remove('active'));
-        this.classList.add('active');
-        filterTodos(this.id);
-    });
+// Additional functionality for deleting a to-do item (not provided in your original code)
+document.getElementById('todo-list').addEventListener('click', function (e) {
+    if (e.target && e.target.matches('li.todo-item')) {
+        e.target.remove();
+    }
 });
-
-function filterTodos(filter) {
-    var todos = document.querySelectorAll('.todo-item');
-    todos.forEach(todo => {
-        switch (filter) {
-            case 'all':
-                todo.style.display = '';
-                break;
-            case 'active':
-                if (todo.classList.contains('completed')) {
-                    todo.style.display = 'none';
-                } else {
-                    todo.style.display = '';
-                }
-                break;
-            case 'completed':
-                if (!todo.classList.contains('completed')) {
-                    todo.style.display = 'none';
-                } else {
-                    todo.style.display = '';
-                }
-                break;
-        }
-    });
-}
 
