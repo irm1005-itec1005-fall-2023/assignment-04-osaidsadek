c3252bcc96a91ba123f20063b2b23d301d7bee0e
osaidsadek <144632318+osaidsadek@users.noreply.github.com>
2023-11-28 21:10:57 +0000

    added code

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
-/* Assignment 04: Finishing a Todo List App
- *
- * 
- *
- */
+document.getElementById('add-todo-btn').addEventListener('click', addTodo);
 
-
-//
-// Variables
-//
-
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
-  }
-
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
+function addTodo() {
+    var input = document.getElementById('todo-input');
+    var text = input.value.trim();
+    if (text) {
+        var li = document.createElement('li');
+        li.className = 'todo-item';
+        li.innerHTML = `<span>${text}</span> <button class="delete-btn">Delete</button>`;
+        li.querySelector('.delete-btn').addEventListener('click', function() {
+            li.remove();
+        });
+        li.addEventListener('click', function() {
+            this.classList.toggle('completed');
+        });
+        document.getElementById('todo-list').appendChild(li);
+        input.value = '';
+    }
 }
 
-//
-// Inits & Event Listeners
-//
-inititialise();
+document.querySelectorAll('.filter-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        document.querySelectorAll('.filter-btn').forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+        filterTodos(this.id);
+    });
+});
+
+function filterTodos(filter) {
+    var todos = document.querySelectorAll('.todo-item');
+    todos.forEach(todo => {
+        switch (filter) {
+            case 'all':
+                todo.style.display = '';
+                break;
+            case 'active':
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'none';
+                } else {
+                    todo.style.display = '';
+                }
+                break;
+            case 'completed':
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'none';
+                } else {
+                    todo.style.display = '';
+                }
+                break;
+        }
+    });
+}
+
